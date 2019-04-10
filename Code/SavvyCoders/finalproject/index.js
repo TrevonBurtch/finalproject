2814911906333c07fd487b71799cfca915134f22
Trevon Burtch <trevonburtch4@msn.com>
2019-04-10 00:58:06 -0500

    Added API Functionality

--- Code/SavvyCoders/finalproject/index.js
+++ Code/SavvyCoders/finalproject/index.js
@@ -1,8 +1,21 @@
+import axios from 'axios';
+console.log('axios');
 axios
-    .get('https://api.mysportsfeeds.com/v2.1/pull/mlb/2019-regular/games.json', {
+    .get('https://api.mysportsfeeds.com/v1.0/pull/mlb/2018-regular/full_game_schedule.json', {
         'headers': {
-            'Authorization': `Basic ${btoa('b0830822-1dcd-4e7b-869b-dd01b8:MYSPORTSFEEDS')}`
-        } }).then(function(response){
-        // handle error
-        console.log(response);
+            'Authorization': `Basic ${btoa('2269d933-29f3-4e97-a745-fec65d:111991')}`
+        } })
+    .then(function(response){
+        let schedule = document.getElementById('schedule');
+        
+        let games = response.data.fullgameschedule.gameentry;
+
+        for(let i = 0; i < games.length; i++){
+            console.log(games[i]);
+
+
+            let html = `<div>${games[i].awayTeam.Name}</div>`;
+            
+            schedule.insertAdjacentHTML('beforeend',html);
+        }
     });
